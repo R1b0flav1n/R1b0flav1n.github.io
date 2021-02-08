deee7add2b2b63c78367d21b6e0372932afca05b
R1b0flav1n <crdickson97@gmail.com>
2021-02-08 18:39:22 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+
+    <title>CISproject1</title>
+</head>
+<body>
+    <!--Menu-->
+    <?php 
+        include("../../ToC.php");
+    ?>
+    
+    <!--Review-->
+    <p>     Welcome to CIS project 1. Website hosting is the bread and butter of maintaining a website, choosing a reliable and trustworthy host is vital. This website is hosted by Github and provides many features and services that are useful. Offering ease of collaboration, back ups, and immense documentation to help resolve any problems.</p>
+    
+    <p>     There are unfortunatley some disadvatages to using Github. Many of Git hubs features are locked behind payment creating a frustrating experience when issues can only be resolved through paid features. Additionally Github is not known for having adequate security and is therefore riskier than other possible services. Regardless of the flaws the hosting platform is worth recommending to those with little interest in running a website without the assistance of a UI that does most of the work for you that is also mostly free.
+    </p>
+    
+    <p>In order to view the PHP info of this assignment, please click <a href="phpinfo.php">here</a>.
+    </p>
+   
+    <!--Footer-->
+    <?php
+        include("../../footer.php");
+    ?>
+</body>
+</html>
