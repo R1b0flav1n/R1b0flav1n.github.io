fcf00f42b14528bcc9c27cf574842ffa40332f29
R1b0flav1n <crdickson97@gmail.com>
2021-02-08 18:39:19 -0500

    Create ToC.php

---
+++ ToC.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+
+</head>
+<body>
+    <div class="submenu">
+                
+                <h1><a href="index.php">About Us</a></h1>
+				
+    </div>
+</body>
+</html>
