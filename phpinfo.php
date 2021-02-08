a277974af85268a112ddddeab0a4f914c7b392ea
R1b0flav1n <crdickson97@gmail.com>
2021-02-08 18:39:14 -0500

    Create phpinfo.php

---
+++ phpinfo.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+ 
+    <title>CISproject1</title>
+</head>
+<body>
+    <!--Menu-->
+    <?php
+        include("../../ToC.php");
+    ?>
+    
+    <!--PHP info-->
+    <?php 
+        phpinfo(); 
+    ?>
+    
+    <!--Footer-->
+    <?php
+        include("../../footer.php");
+    ?>
+</body>
+</html>
